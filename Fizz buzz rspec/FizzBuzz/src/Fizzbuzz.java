569c915e1a9f731d24cfdfe9001c7eb952f45e60
Diego <50609792+DiegoViscarra@users.noreply.github.com>
2020-02-11 12:36:09 -0400

    refactorizado

--- Fizz buzz rspec/FizzBuzz/src/Fizzbuzz.java
+++ Fizz buzz rspec/FizzBuzz/src/Fizzbuzz.java
@@ -1,20 +1,29 @@
 
 public class Fizzbuzz {
+	private static final int _BUZZ = 5;
+	private static final int _FIZZ = 3;
+
+
 	void generarMuchos(){
 		for(int i = 0; i < 100; i++)
 		{
-			System.out.println(generarPara(i));
+			System.out.println(generar(i));
         }
 	}
 	
-	public String generarPara(int numero){
-		if (numero % 3 == 0 && numero % 5 == 0)
-			return("FizzBuzz");                    
-		if(numero % 3 == 0)
+	public String generar(int numero){
+		if (esMultiploDe(numero, _FIZZ) && esMultiploDe(numero, _BUZZ))
+			return("FizzBuzz");     
+		if(esMultiploDe(numero, _FIZZ))
 			return("Fizz");
-		if(numero % 5 == 0)
+		if(esMultiploDe(numero, _BUZZ))
 			return("Buzz");
 		else
 			return(Integer.toString(numero));
+	}
+
+       
+	private boolean esMultiploDe(int numero, int multiplo) {
+		return numero % multiplo == 0;
 	}
 }
--- Fizz buzz rspec/FizzBuzz/src/ProgramTest.java
+++ Fizz buzz rspec/FizzBuzz/src/ProgramTest.java
@@ -1,18 +1,27 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ProgramTest {
 
+	Fizzbuzz fizzbuzz = new Fizzbuzz();
+	@Before
+	public void setup() {
+		fizzbuzz=new Fizzbuzz();
+	}
 	@Test
 	public void test() {
-		Fizzbuzz test= new Fizzbuzz();
-		assertEquals("Deberia devolver FizzBuzz", test.generarPara(0),"FizzBuzz");
-		assertEquals("Deberia devolver 1", test.generarPara(1),"1");
-		assertEquals("Deberia devolver 2", test.generarPara(2),"2");
-		assertEquals("Deberia devolver Fizz", test.generarPara(3),"Fizz");
-		assertEquals("Deberia devolver 4", test.generarPara(4),"4");
-		assertEquals("Deberia devolver Buzz", test.generarPara(5),"Buzz");
-		assertEquals("Deberia devolver FizzBuzz", test.generarPara(15),"FizzBuzz");
+		assertEquals("Deberia devolver FizzBuzz", fizzbuzz.generar(0),"FizzBuzz");
+		assertEquals("Deberia devolver 1", fizzbuzz.generar(1),"1");
+		assertEquals("Deberia devolver 2", fizzbuzz.generar(2),"2");
+		assertEquals("Deberia devolver Fizz", fizzbuzz.generar(3),"Fizz");
+		assertEquals("Deberia devolver 4", fizzbuzz.generar(4),"4");
+		assertEquals("Deberia devolver Buzz", fizzbuzz.generar(5),"Buzz");
+		assertEquals("Deberia devolver FizzBuzz", fizzbuzz.generar(15),"FizzBuzz");
+	}
+	@Test
+	public void ComprobarMultiplosDe5() {	
+		assertEquals(fizzbuzz.generar(5),"Buzz");
 	}
 }
