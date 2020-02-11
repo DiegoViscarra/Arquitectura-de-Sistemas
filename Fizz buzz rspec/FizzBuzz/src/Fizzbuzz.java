51617bdbdb64bd50584607103f02d36b42b56a74
Diego <50609792+DiegoViscarra@users.noreply.github.com>
2020-02-11 10:56:31 -0400

    sin anidado

--- Fizz buzz rspec/FizzBuzz/src/Fizzbuzz.java
+++ Fizz buzz rspec/FizzBuzz/src/Fizzbuzz.java
@@ -9,20 +9,12 @@ public class Fizzbuzz {
 	
 	public String generarPara(int numero){
 		if (numero % 3 == 0 && numero % 5 == 0)
-		{
 			return("FizzBuzz");                    
-		}
-		else 
-			if(numero % 3 == 0)
-			{
-				return("Fizz");
-			}
-			else 
-				if(numero % 5 == 0)
-				{
-					return("Buzz");
-				}
-				else
-					return(Integer.toString(numero));
+		if(numero % 3 == 0)
+			return("Fizz");
+		if(numero % 5 == 0)
+			return("Buzz");
+		else
+			return(Integer.toString(numero));
 	}
 }
